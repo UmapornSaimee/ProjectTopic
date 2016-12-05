8d871e417dc87630d36e06ccf9ec8ea8a0937226
CZEM51UDSTPEV6L\USER <umaporn_saimee@kkumail.com>
2016-12-05 20:23:09 +0700

    โปรแกรมคำนวณอายุ

---
+++ ProjectTopic/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ProjectTopic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblBirth = new System.Windows.Forms.Label();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.btnClaer = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtName.Location = new System.Drawing.Point(111, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblName.Location = new System.Drawing.Point(70, 43);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(26, 20);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "ชื่อ";
+            // 
+            // lblBirth
+            // 
+            this.lblBirth.AutoSize = true;
+            this.lblBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblBirth.Location = new System.Drawing.Point(54, 83);
+            this.lblBirth.Name = "lblBirth";
+            this.lblBirth.Size = new System.Drawing.Size(42, 20);
+            this.lblBirth.TabIndex = 4;
+            this.lblBirth.Text = "ปีเกิด";
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnSum.Location = new System.Drawing.Point(125, 160);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(186, 34);
+            this.btnSum.TabIndex = 5;
+            this.btnSum.Text = "คำนวณ";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblYear.Location = new System.Drawing.Point(35, 123);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(61, 20);
+            this.lblYear.TabIndex = 6;
+            this.lblYear.Text = "ปีปัจจุบัน";
+            // 
+            // btnClaer
+            // 
+            this.btnClaer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnClaer.Location = new System.Drawing.Point(274, 216);
+            this.btnClaer.Name = "btnClaer";
+            this.btnClaer.Size = new System.Drawing.Size(37, 33);
+            this.btnClaer.TabIndex = 8;
+            this.btnClaer.Text = "ลบ";
+            this.btnClaer.UseVisualStyleBackColor = true;
+            this.btnClaer.Click += new System.EventHandler(this.btnClaer_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 83);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(121, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 11;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtResult.Location = new System.Drawing.Point(12, 216);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(238, 33);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 277);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnClaer);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.lblBirth);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "โปรแกรมคำนวณอายุ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblBirth;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Button btnClaer;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ ProjectTopic/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectTopic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            string Name = Convert.ToString(txtName.Text); //ประกาศตัวแปลรับชื่อเข้ามา
+            string date = DateTime.Now.ToString("dd-MM-yyyy"); //วันเดือนปีปัจจุบัน
+            DateTime now = DateTime.Now; 
+            DateTime select = dateTimePicker1.Value; //ประกาศตัวแปลรับค่าวันเดือนปีเกิด
+            txtResult.Text = "คุณ    "+Name+"    มีอายุ    "+ CalAge(now, select).ToString()+"  ปี"; //แสดงผล
+
+
+        }
+        //คำนวณอายุ
+        public long CalAge(System.DateTime now,System.DateTime select)
+        {
+            long age = 0;  //ประกาศตัวแปล
+            System.TimeSpan ts = new TimeSpan(now.Ticks - select.Ticks); //ปีปัจจุบันลบด้วยค่าวันเดือนปีที่เรารับมา 
+            age = (long)(ts.Days / 365); //วันที่ ที่ลบกันออกมาแร้วมาหารด้วย 365 วัน
+
+            return age;
+        }
+        //คำสั่งลบ
+        private void btnClaer_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtResult.Clear();
+        }
+        //เวลารันโปรแกรมขึ้นมาให้วันเดือนปีไปโชร์ที่ Labell เฉยๆค่ะ
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString("dd-MM-yyyy"); 
+            label1.Text = date.ToString();
+        }
+    }
+}
